7865c7a24265ce1a3503a672b1e224e273afa263
Lara Aasem <lara-aasem@hotmail.com>
2018-01-14 05:36:24 +0200

    fix most of about page pending no scroll

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './About.css';
 
-
 class About extends Component {
   componentWillMount() {
     const { handleNewSearchInput } = this.props;
@@ -10,19 +9,24 @@ class About extends Component {
 
   render() {
     return(
-      <div className="main">
-        <div class="aboutpage">
-        <h1>Almakinah SUMMIT</h1>
-        <p>The summit is our one-stop-shop, entrepreneurship marathon! 
-            We promise that the world comes to Almakinah, 
-            and thanks to an incredible level of commitment from our tireless team and partners, we deliver. 
-            Thought leaders, media mavens, tech-legends and power player investors gather to share their experience,
-            support and invest in the Middle East & Africa’s flourishing entrepreneurial ecosystem.
-            From workshops, inspiring talks, and panel discussions to startup stations, pitch competitions,
-            and networking platforms with industry experts. Whoever you are, you’ll find a place for you.</p>
-            <p>NOW!! You can choose from various ticket types and pay online</p>
+      <div className="about">
+        <div className="page">
+          <div className="about-container">
+            <div className="about-content">
+              <h2>About the Summit</h2>
+              <div className="about-paragraphs">
+                <p>
+                    The summit is our one-stop-shop, entrepreneurship marathon! 
+                    We promise that the world comes to AlMakinah, 
+                    and thanks to an incredible level of commitment from our tireless team and partners, we deliver. 
+                    Thought leaders, media mavens, tech-legends and power player investors gather to share their experience,
+                    support and invest in the Middle East & Africa’s flourishing entrepreneurial ecosystem.
+                    From workshops, inspiring talks, and panel discussions to startup stations, pitch competitions,
+                    and networking platforms with industry experts. Whoever you are, you’ll find a place for you.</p>
+              </div>
+            </div>
+          </div>
         </div>
-        
       </div>
       );
   }
